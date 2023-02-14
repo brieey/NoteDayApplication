2e6dcb57506062c7f6498639df382482469fbcf2
brieey <briannanichole247@gmail.com>
2023-02-14 14:37:59 -0600

    Initial commit

---
+++ src/main/java/com/springbootapp/noteday/NoteDayApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapp.noteday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteDayApplication.class, args);
+    }
+
+}
